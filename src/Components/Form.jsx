0efb67f4e58b1afeaddd0282e9454dd902012e3e
Benas <benas.sarachovas@gmail.com>
2023-08-10 12:25:24 +0100

    using react-hook-form to validate form input, added error messages

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,38 +1,29 @@
-import { useState } from "react";
-
+import { useForm } from "react-hook-form"
 
 export default function Form(){
     
-  const [name, setName] = useState("")
-  const [rate, setRate] = useState("")
-  const [comment, setComment] = useState("")
+  const {register, handleSubmit, formState: {errors}} = useForm()
 
-  const handleForm = (e) =>{
-    e.preventDefault()
-
-    if(!name.match(/^[A-Za-z\s]+$/)){
-        alert("Your name is using invalid symbols or is empty")
-    }
-
-    if(Number(rate) <= 5 && comment.length < 5){
-        alert("Let us know why your experience was poor")
-    }
+  const submittedData = (data) =>{
+    console.log(data)
   }
+  console.log(errors)
 
   return(
-    <form onSubmit={handleForm}>
-        <fieldset>
-            <label htmlFor="name">Your name: </label>
-            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}></input>
-
-            <label htmlFor="rate">Rate your experience: </label>
-            <input type="range" min="0" max="10" id="rate" value={rate} onChange={(e) => setRate(e.target.value)}></input>
-
-            <label htmlFor="comment">Leave a comment: </label>
-            <textarea id="comment" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
-        </fieldset>
-
-        <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit(submittedData)}>
+      <fieldset>
+        <label htmlFor="fName">First name: </label>
+        <input {...register("firstName", {
+          pattern: { //Pattern validation
+            value: /^[A-Za-z\s]+$/, //Pattern
+            message: "Using invalid symbols!" //Error message
+          },
+          required: "Required field!" //Error message
+        })}
+        id="fName"></input>
+        <p>{errors.firstName?.message}</p>
+      </fieldset>
+      <button type="submit">Submit</button>
     </form>
   )
 }
